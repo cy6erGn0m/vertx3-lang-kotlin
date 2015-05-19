a033be8a704c89e339b3e9c84df315a5416d4ae6
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2015-05-19 21:09:53 +0300

    DNS client bindings

---
+++ src/examples/kotlin/dnsExamples.kt
@@ -0,0 +1,153 @@
+package examples
+
+import io.vertx.kotlin.lang.*
+import io.vertx.core.Vertx
+import io.vertx.core.dns.DnsClient
+import io.vertx.core.dns.MxRecord
+import io.vertx.core.dns.SrvRecord
+import io.vertx.kotlin.lang.AsyncSuccessResult
+
+/**
+ * @author [Julien Viet](mailto:julien@julienviet.com)
+ */
+public class DNSExamples {
+
+    public fun example1(vertx: Vertx) {
+        val client = vertx.createDnsClient(53, "10.0.0.1")
+    }
+
+    public fun example2(vertx: Vertx) {
+        val client = vertx.createDnsClient(53, "10.0.0.1")
+        client.lookup("vertx.io") { result ->
+            when (result) {
+                is AsyncSuccessResult -> println("Resolved to ${result.result}")
+                is AsyncErrorResult -> println("Failed to resolve due to ${result.error}")
+            }
+        }
+    }
+
+    public fun example3(vertx: Vertx) {
+        val client = vertx.createDnsClient(53, "10.0.0.1")
+        client.lookup4("vertx.io") { result ->
+            when (result) {
+                is AsyncSuccessResult -> println("Resolved to ${result.result}")
+                is AsyncErrorResult -> println("Failed to resolve due to ${result.error}")
+            }
+        }
+    }
+
+    public fun example4(vertx: Vertx) {
+        val client = vertx.createDnsClient(53, "10.0.0.1")
+        client.lookup6("vertx.io") { result ->
+            when (result) {
+                is AsyncSuccessResult -> println("Resolved to ${result.result}")
+                is AsyncErrorResult -> println("Failed to resolve due to ${result.error}")
+            }
+        }
+    }
+
+    public fun example5(vertx: Vertx) {
+        val client = vertx.createDnsClient(53, "10.0.0.1")
+        client.resolveA("vertx.io") { result ->
+            when (result) {
+                is AsyncSuccessResult -> result.result.forEach { println(it) }
+                is AsyncErrorResult -> println("Failed to resolve due to ${result.error}")
+            }
+        }
+    }
+
+    public fun example6(vertx: Vertx) {
+        val client = vertx.createDnsClient(53, "10.0.0.1")
+        client.resolveAAAA("vertx.io") { result ->
+            when (result) {
+                is AsyncSuccessResult -> result.result.forEach { println(it) }
+                is AsyncErrorResult -> println("Failed to resolve due to ${result.error}")
+            }
+        }
+    }
+
+    public fun example7(vertx: Vertx) {
+        val client = vertx.createDnsClient(53, "10.0.0.1")
+        client.resolveCNAME("vertx.io") { result ->
+            when (result) {
+                is AsyncSuccessResult -> result.result.forEach { println(it) }
+                is AsyncErrorResult -> println("Failed to resolve due to ${result.error}")
+            }
+        }
+    }
+
+    public fun example8(vertx: Vertx) {
+        val client = vertx.createDnsClient(53, "10.0.0.1")
+        client.resolveMX("vertx.io") { result ->
+            when (result) {
+                is AsyncSuccessResult -> result.result.forEach { println(it) }
+                is AsyncErrorResult -> println("Failed to resolve due to ${result.error}")
+            }
+        }
+    }
+
+    public fun example9(record: MxRecord) {
+        record.priority()
+        record.name()
+    }
+
+    public fun example10(vertx: Vertx) {
+        val client = vertx.createDnsClient(53, "10.0.0.1")
+        client.resolveTXT("vertx.io") { result ->
+            when (result) {
+                is AsyncSuccessResult -> result.result.forEach { println(it) }
+                is AsyncErrorResult -> println("Failed to resolve due to ${result.error}")
+            }
+        }
+    }
+
+    public fun example11(vertx: Vertx) {
+        val client = vertx.createDnsClient(53, "10.0.0.1")
+        client.resolveNS("vertx.io") { result ->
+            when (result) {
+                is AsyncSuccessResult -> result.result.forEach { println(it) }
+                is AsyncErrorResult -> println("Failed to resolve due to ${result.error}")
+            }
+        }
+    }
+
+    public fun example12(vertx: Vertx) {
+        val client = vertx.createDnsClient(53, "10.0.0.1")
+        client.resolveSRV("vertx.io") { result ->
+            when (result) {
+                is AsyncSuccessResult -> result.result.forEach { println(it) }
+                is AsyncErrorResult -> println("Failed to resolve due to ${result.error}")
+            }
+        }
+    }
+
+    public fun example13(record: SrvRecord) {
+        record.priority()
+        record.name()
+        record.weight()
+        record.port()
+        record.protocol()
+        record.service()
+        record.target()
+    }
+
+    public fun example14(vertx: Vertx) {
+        val client = vertx.createDnsClient(53, "10.0.0.1")
+        client.resolvePTR("1.0.0.10.in-addr.arpa") { result ->
+            when (result) {
+                is AsyncSuccessResult -> println(result.result)
+                is AsyncErrorResult -> println("Failed to resolve due to ${result.error}")
+            }
+        }
+    }
+
+    public fun example15(vertx: Vertx) {
+        val client = vertx.createDnsClient(53, "10.0.0.1")
+        client.reverseLookup("10.0.0.1") { result ->
+            when (result) {
+                is AsyncSuccessResult -> println(result.result)
+                is AsyncErrorResult -> println("Failed to resolve due to ${result.error}")
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/io/vertx/kotlin/lang/dns-client.kt
@@ -0,0 +1,44 @@
+package io.vertx.kotlin.lang
+
+import io.vertx.core.Handler
+import io.vertx.core.dns.DnsClient
+import io.vertx.core.dns.MxRecord
+import io.vertx.core.dns.SrvRecord
+
+public fun DnsClient.lookup(a: String, handler: (AsyncResult<String>) -> Unit) : DnsClient =
+        lookup(a, Handler { handler(it.toAsyncResultK()) })
+
+public fun DnsClient.lookup4(a: String, handler: (AsyncResult<String>) -> Unit) : DnsClient =
+        lookup4(a, Handler { handler(it.toAsyncResultK()) })
+
+public fun DnsClient.lookup6(a: String, handler: (AsyncResult<String>) -> Unit) : DnsClient =
+        lookup6(a, Handler { handler(it.toAsyncResultK()) })
+
+public fun DnsClient.resolveA(a: String, handler: (AsyncResult<List<String>>) -> Unit) : DnsClient =
+        resolveA(a, Handler { handler(it.toAsyncResultK()) })
+
+public fun DnsClient.resolveAAAA(a: String, handler: (AsyncResult<List<String>>) -> Unit) : DnsClient =
+        resolveAAAA(a, Handler { handler(it.toAsyncResultK()) })
+
+public fun DnsClient.resolveCNAME(a: String, handler: (AsyncResult<List<String>>) -> Unit) : DnsClient =
+        resolveCNAME(a, Handler { handler(it.toAsyncResultK()) })
+
+public fun DnsClient.resolveTXT(a: String, handler: (AsyncResult<List<String>>) -> Unit) : DnsClient =
+        resolveTXT(a, Handler { handler(it.toAsyncResultK()) })
+
+public fun DnsClient.resolvePTR(a: String, handler: (AsyncResult<String>) -> Unit) : DnsClient =
+        resolvePTR(a, Handler { handler(it.toAsyncResultK()) })
+
+public fun DnsClient.resolveNS(a: String, handler: (AsyncResult<List<String>>) -> Unit) : DnsClient =
+        resolveNS(a, Handler { handler(it.toAsyncResultK()) })
+
+public fun DnsClient.reverseLookup(a: String, handler: (AsyncResult<String>) -> Unit) : DnsClient =
+        reverseLookup(a, Handler { handler(it.toAsyncResultK()) })
+
+public fun DnsClient.resolveSRV(a: String, handler: (AsyncResult<List<SrvRecord>>) -> Unit) : DnsClient =
+        resolveSRV(a, Handler { handler(it.toAsyncResultK()) })
+
+public fun DnsClient.resolveMX(a: String, handler: (AsyncResult<List<MxRecord>>) -> Unit) : DnsClient =
+        resolveMX(a, Handler { handler(it.toAsyncResultK()) })
+
+
