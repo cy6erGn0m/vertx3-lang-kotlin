a64bf2eb50587ddfc35bc7a18eed1da8681579f2
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2015-07-09 16:23:02 +0300

    Add Chuck Norris example

---
+++ src/examples/kotlin/chucknorris.kt
@@ -0,0 +1,22 @@
+package examples.chucknorris
+
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.vertx.kotlin.lang.DefaultVertx
+
+/**
+ * -This example connects to the server and retrieves random joke about Chuck Norris-
+ * Chuck Norris sending us a random joke and we display it on your screen when he want
+ */
+fun main(args: Array<String>) {
+    DefaultVertx {
+        createHttpClient().get("api.icndb.com", "/jokes/random") { response ->
+            response.bodyHandler {
+                val node = ObjectMapper().readValue(it.getBytes(), javaClass<JsonNode>())
+                val joke = node.get("value").get("joke").asText()
+                println(joke)
+                this.close()
+            }
+        }.end()
+    }
+}
